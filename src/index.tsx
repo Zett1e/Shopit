bc8a6d54a8daa9a03da63ecb572a980a140133bd
Thaw Zin <thawzzin.16@gmail.com>
2023-08-11 23:50:37 +0630

    fix:remove gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/shopit">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
